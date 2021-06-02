c09bfe3a32910d71dd89f12ad387c705ec8370c2
harrietfiagbor <nathaniel.asiak@acity.edu.gh>
2021-06-02 03:19:30 +0000

    added code for flame and smoke detection

--- app.c
+++ app.c
@@ -1,14 +1,14 @@
 // Program for LCD Interfacing with 8051 Microcontroller (AT89S52) 
-#include<reg51.h>
+#include <reg51.h>
 #define display_port P2      //Data pins connected to port 2 on microcontroller
 sbit rs = P3^2;  //RS pin connected to pin 2 of port 3
 sbit rw = P3^3;  // RW pin connected to pin 3 of port 3
 sbit e =  P3^4;  //E pin connected to pin 4 of port 3
-sbit buzzer = P0^0 //buzzer connected to port 0 bit 0
-sbit fsensor = P1^6 //connection of flame sensor
-sbit motor =  P1^7 //motor connection
-sbit gsensor = P1^4  //gas sensor connection
-sbit LED = P0^2     //bulb connection
+sbit buzzer = P0^0; //buzzer connected to port 0 bit 0
+sbit fsensor = P1^6; //connection of flame sensor
+sbit motor = P1^7;   //motor connection
+sbit gsensor = P1^4; //gas sensor connection
+sbit LED = P0^2;     //bulb connection
 
 void msdelay(unsigned int time)  // Function for creating delay in milliseconds.
 {
@@ -52,28 +52,50 @@ void flame_detection() //function  for flame detection
 {
         if(fsensor) {
             cmd(0xc0);
-            lcd_string("Flame Detected");
+            lcd_string("Smoke and Fire Detected");
             delay();
-            buzzer() ;//call the buzzer function
-            pump() ;   //call the turn on pump function
-            bulb();    //call the turn on bulb function  
+            buzzer = 1;//call the buzzer function
+            pump = 1;   //call the turn on pump function
+            bulb = 1;    //call the turn on bulb function  
         } else {
             cmd(0xc0);
             lcd_string("                ");
         }
+
+}
+
+void smoke_detection(){
+    if (gsensor==1){
+        cmd(0xc0);
+            lcd_string("smoke Detected");
+            delay();
+            buzzer = 1;
+            pump = 1; 
+        } else {
+            buzzer = 0;
+            pump = 0;
+            cmd(0xc0);
+            lcd_string("                ");
+        }
+ }
+
 void main()
 {
     buzzer = 0
     fsensor = 0
     gsensor = 0
-    unsigned char a[15]="CIRCUIT DIGEST";    //string of 14 characters with a null terminator.
-    int l=0;
+    unsigned char a[17]="Welcome";    //string of 14 characters with a null terminator.
+    int l = 0;
     lcd_init();
     while(a[l] != '\0') // searching the null terminator in the sentence
     {
         lcd_data(a[l]);
-        l++;
+        l++ ;
         msdelay(50);
+    }
+    while(1){
+        flame_detection();
+        smoke_detection();
     }
 }
 
